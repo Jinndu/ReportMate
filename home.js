cd01dfb303eecce0bbac1acb97fd9212294527e5
Jinndu <angelobliss178@gmail.com>
2026-07-14 21:13:17 +0100

    Side bar filter effect

--- home.js
+++ home.js
@@ -2,6 +2,8 @@
 const navMenu = document.querySelector("nav .nav-menu")
 const asideMenu = document.querySelector("aside")
 const closeMenuButton = document.querySelector("aside span")
+const main = document.querySelector("main")
+const body = document.querySelector("body")
 
 // EVENTS
 navMenu.addEventListener("click",openMenu)
@@ -10,7 +12,12 @@ closeMenuButton.addEventListener("click",closeSideMenu)
 // FUNCTION FOR EVENTS
 function openMenu(){
     asideMenu.classList.add("side-menu-slide-In")
+    main.classList.add("menu-open")
+    body.style.overflow = "hidden"
 }
 function closeSideMenu(){
     asideMenu.classList.remove("side-menu-slide-In")
+    main.classList.remove("menu-open")
+    body.style.overflow="auto"
+    
 }
